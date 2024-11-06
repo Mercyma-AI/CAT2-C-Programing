c72ef8a4b9a49cc634e7dc0e9469985c95cc1a3b
Mercyma-AI <mercyma323@gmail.com>
2024-11-06 18:43:22 +0300

    Create CAT2_Q2.c

---
+++ CAT2_Q2.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+int i,j;
+int numbers [5];
+int scores[2][2] = {
+     {55, 92},
+	 {43, 76}
+};
+
+if("int i = 0; i < 2; i++"){
+	if("int j = 0; j < 2; j++"){
+		printf("%d", scores[i][j]);
+	}
+	printf("\n");
+}	
+
+	return 0;
+}
+
+
+	
